4a38370ab4966b2f2d237240cb393e2090e1c7ac
Daiyangcheng <daiyangcheng@locyan.cn>
2025-08-10 23:50:17 +0800

    Support STOMP over SockJS

--- main.go
+++ main.go
@@ -2,137 +2,18 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"github.com/gorilla/websocket"
-	"go.uber.org/zap"
 	"lcf-controller/inject"
 	"lcf-controller/logger"
-	"lcf-controller/net/server"
 	"lcf-controller/pkg/config"
-	"lcf-controller/pkg/info"
-	_type "lcf-controller/pkg/type/frps"
+	websocket2 "lcf-controller/pkg/websocket"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 	"time"
+
+	"go.uber.org/zap"
 )
-
-// NewWebSocket 初始化WebSocket客户端
-func NewWebSocket() *WsClient {
-	ws := new(WsClient)
-	cfg := config.ReadCfg()
-	ws.addr = cfg.ControllerConfig.Addr
-	return ws
-}
-
-// ConnectWsServer 连接到WebSocket服务器
-func (w *WsClient) ConnectWsServer() (err error) {
-	conn, _, err := websocket.DefaultDialer.Dial(w.addr, nil)
-	if err != nil {
-		return err
-	}
-	w.conn = conn
-	return nil
-}
-
-// SendMsg 发送消息到服务器
-func (w *WsClient) SendMsg(cfg *config.Config, action string, data map[string]any) (err error) {
-	req := new(BasicRequest)
-	req.Action = action
-	req.Node.Id = cfg.ControllerConfig.NodeId
-	req.Node.ApiKey = cfg.ControllerConfig.NodeApiKey
-	req.Data = data
-	msg, err := json.Marshal(req)
-	err = w.conn.WriteMessage(websocket.TextMessage, msg)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// ReadMsg 从服务器读取消息
-func (w *WsClient) ReadMsg() {
-	defer func() {
-		err := w.conn.Close()
-		if err != nil {
-			logger.Error("error closing connection", zap.Error(err))
-		}
-	}()
-
-	for {
-		_, msg, err := w.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				logger.Error("error reading message", zap.Error(err))
-			}
-			break
-		}
-		var msgJson WsResponse
-		err = json.Unmarshal(msg, &msgJson)
-		if err != nil {
-			logger.Error("can't unmarshal json message", zap.Error(err))
-		}
-		switch msgJson.Status {
-		case 200:
-			logger.Debug("received message from server", zap.String("msg", string(msg)))
-		case 401:
-			logger.Fatal("API reported unauthorized")
-		default:
-			logger.Error("error message from server", zap.String("msg", string(msg)))
-		}
-	}
-}
-
-func (w *WsClient) sendNodeStatsToServer(cfg *config.Config, serverInfo _type.ServerInfoResponse) {
-	// nodeInfo
-	err := w.SendMsg(cfg, "upload-node-stats", info.GetNodeInfo(serverInfo))
-	if err != nil {
-		logger.Error("send node info to server failed!", zap.Error(err))
-	}
-}
-
-func (w *WsClient) sendProxyStatsToServer(cfg *config.Config) {
-	types := []string{"tcp", "udp", "http", "https", "xtcp", "stcp"}
-	for _, p := range types {
-		proxies, err := info.GetProxies(p)
-		if err != nil {
-			logger.Error("can't request proxies info", zap.Error(err))
-		} else {
-			for _, j := range proxies {
-				err := w.SendMsg(cfg, "upload-proxy-stats", j)
-				logger.Info("send proxy info to the server")
-				if err != nil {
-					logger.Error("send proxy info to server failed!", zap.Error(err))
-				}
-			}
-		}
-	}
-}
-
-// WsClient WebSocket客户端结构
-type WsClient struct {
-	addr string
-	conn *websocket.Conn
-}
-
-type WsResponse struct {
-	Status  int             `json:"status"`
-	Message string          `json:"message"`
-	Data    json.RawMessage `json:"data"`
-}
-
-type BasicRequest struct {
-	Action string         `json:"action"`
-	Node   NodeInfo       `json:"node"`
-	Data   map[string]any `json:"data"`
-}
-
-type NodeInfo struct {
-	Id     int    `json:"id"`
-	ApiKey string `json:"api_key"`
-}
 
 func createContext() (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -143,12 +24,7 @@ func createContext() (context.Context, context.CancelFunc) {
 		signal.Notify(shutdownChan, os.Interrupt, syscall.SIGTERM)
 		<-shutdownChan
 		logger.Info("shutting down gracefully...")
-
-		logger.Info("stopping workers...")
 		cancel()
-		logger.Info("engine stopped")
-
-		os.Exit(0)
 	}()
 	return ctx, cancel
 }
@@ -170,7 +46,7 @@ func main() {
 	}
 
 	if cfg.ControllerConfig.Enable {
-		ws := NewWebSocket()
+		ws := websocket2.NewWebSocket(ctx)
 		logger.Info("connecting to WebSocket endpoint...")
 		err := ws.ConnectWsServer()
 		if err != nil {
@@ -180,40 +56,37 @@ func main() {
 			)
 		} else {
 			logger.Info("connect to WebSocket server successfully")
-			defer func(conn *websocket.Conn) {
-				err := conn.Close()
+
+			// 链接成功后就可以开始接收消息了
+			// 异步处理
+			go func() {
+				err := ws.ReadMsg()
 				if err != nil {
-					logger.Fatal(
-						"can't close WebSocket connection",
-						zap.Error(err),
-					)
+					logger.Fatal("Cannot read message", zap.Error(err))
 				}
-			}(ws.conn)
-			go ws.ReadMsg()
+			}()
+			defer ws.Disconnect()
+
+			// 订阅消息
+			err := ws.Subscribe("/traffic")
+			if err != nil {
+				logger.Fatal("Cannot subscribe traffic", zap.Error(err))
+			}
+
+			// 先发一个
+			ws.SendProxyStatsToServer(cfg)
 			ticker := time.NewTicker(cfg.ControllerConfig.SendDuration)
 			defer ticker.Stop()
 
-			serverInfo, err := server.GetServerInfo()
-			if err != nil {
-				logger.Error("can't get server info", zap.Error(err))
-			} else {
-				ws.sendNodeStatsToServer(cfg, serverInfo)
-				ws.sendProxyStatsToServer(cfg)
-			}
-
-			for range ticker.C {
-				if err != nil {
-					logger.Error("can't get server info", zap.Error(err))
-				} else {
-					ws.sendNodeStatsToServer(cfg, serverInfo)
-					ws.sendProxyStatsToServer(cfg)
+			for {
+				select {
+				case <-ctx.Done():
+					logger.Info("shutting down...")
+					return
+				case <-ticker.C:
+					ws.SendProxyStatsToServer(cfg)
 				}
 			}
-		}
-	} else {
-		ticker := time.NewTicker(cfg.ControllerConfig.SendDuration)
-		defer ticker.Stop()
-		for range ticker.C {
 		}
 	}
 }
